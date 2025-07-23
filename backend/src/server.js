4f9eff32dc581b5a8d719ab64cbee6aee7fe4121
frenemy17 <raikarsiddhanth@gmail.com>
2025-07-23 23:49:28 +0530

    connect with friends endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,13 @@ const app = express();
 const PORT=process.env.PORT 
 
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
 
 app.use('/api/auth',authRoutes );
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB(); // Ensure the database connection is established
