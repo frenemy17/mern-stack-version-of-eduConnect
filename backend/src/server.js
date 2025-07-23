a0b0a249b1ca6adf879dbf40924d63a7e4f90fc3
frenemy17 <raikarsiddhanth@gmail.com>
2025-07-24 02:40:51 +0530

    all required endpoints working

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,15 @@ const PORT=process.env.PORT
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
+import chatRoutes from "./routes/chat.route.js";
+
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
 
 app.use('/api/auth',authRoutes );
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB(); // Ensure the database connection is established
